4619c12856f33c59c488d9364d0ecdc068dcc206
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-06-10 00:43:24 +0545

    Precision

---
+++ DMA.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int* num = new int;
+    delete num;
+
+
+
+    return 0;
+}
--- preandwid.cpp
+++ preandwid.cpp
@@ -7,7 +7,8 @@ int main(){
     cout << setw(5)<< 0721 << endl;
     cout << setprecision(4)<<21.43748374 << endl; //Set precision le cahi decimal aagi dekhi ko cahi count garera banauxa like 21.43 = 21.43
     cout << "Hello"<<endl << "Aayushmaa"<<endl;
-    cout <<"How are you!?";
+    cout <<"How are you!?" << endl;
+    cout <<fixed << setprecision(4)<<21.43748374 << endl;
 
 
     return 0;
