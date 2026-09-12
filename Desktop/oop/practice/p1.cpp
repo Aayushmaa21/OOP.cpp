77f510b506ac0274e743b66719ad4e91fce50125
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-09-12 20:07:49 +0545

    RETURNING fron obj

---
+++ Desktop/oop/practice/p1.cpp
@@ -0,0 +1,42 @@
+//obj returning from obj
+#include <iostream>
+using namespace std;
+
+class Number
+{
+    int value;
+
+public:
+    void input()
+    {
+        cout << "Enter value: ";
+        cin >> value;
+    }
+
+    Number add(Number n)
+    {
+        Number temp;
+        temp.value = value + n.value; 
+
+        return temp;
+    }
+
+    void display()
+    {
+        cout << "Sum = " << value;
+    }
+};
+
+int main()
+{
+    Number n1, n2, n3;
+
+    n1.input();
+    n2.input();
+
+    n3 = n1.add(n2);
+
+    n3.display();
+
+    return 0;
+}
